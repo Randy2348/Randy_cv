cf6919c144d8c559c70aae2dc464d880c93e2d62
RandyRandomx86 <randy2348.rj@gmail.com>
2022-07-12 15:43:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 const form = document.getElementById("form");
 
 form.addEventListener("submit", () =>
-  emailjs.sendForm("RandomXD", "portfolioTemplate", form).then(
-    function (response) {
-      alert("SUCCESS!", response.status, response.text);
-    },
-    function (error) {
-      alert("FAILED...", error);
-    }
-  )
+  emailjs
+    .sendForm("RandomXD", "portfolioTemplate", form, "nbmF2JUCXWMf0Za2n")
+    .then(
+      function (response) {
+        alert("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        alert("FAILED...", error);
+      }
+    )
 );
-
-var templateParams = {
-  name: "James",
-  email: "randy2348.rj@gmail.com",
-  message: "Check this out!",
-};
-
-// emailjs.send("RandomXD", "portfolioTemplate", templateParams).then(
-//   function (response) {
-//     console.log("SUCCESS!", response.status, response.text);
-//   },
-//   function (error) {
-//     console.log("FAILED...", error);
-//   }
-// );
